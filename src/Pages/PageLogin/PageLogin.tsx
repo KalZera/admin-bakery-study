2a6e148c16f678856b3763422fc4b3e9a8cc9c34
Alexander Karuzoo <kalex.codr@gmail.com>
2020-10-06 11:13:47 -0300

    estrutura projeto

---
+++ src/Pages/PageLogin/PageLogin.tsx
@@ -0,0 +1,11 @@
+import React, { FunctionComponent } from "react";
+
+interface Props {}
+
+export const PageLogin: FunctionComponent<Props> = () => {
+  return (
+    <>
+      <h1>PageLogin</h1>
+    </>
+  );
+};
