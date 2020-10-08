a775e47c5af656098c67255128f8f716a5edb1bd
Alexander Karuzoo <kalex.codr@gmail.com>
2020-10-08 13:56:20 -0300

    criando component lista de musicas

---
+++ src/Pages/Components/ListSong/ListSong.tsx
@@ -0,0 +1,15 @@
+import React, { FunctionComponent } from "react";
+
+import { Container } from "./styles";
+
+interface Props {}
+
+export const ListSong: FunctionComponent<Props> = () => {
+  return (
+    <>
+      <Container>
+        <p>faixa 1 </p>
+      </Container>
+    </>
+  );
+};
